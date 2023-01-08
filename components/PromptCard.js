dd8f1e32e3bb2437cf32c40e88a984219ac717ea
Yuvraj Chandra <Singhyuvraj179@gmail.com>
2023-01-08 14:21:30 +0530

    updated card image

--- components/PromptCard.js
+++ components/PromptCard.js
@@ -6,7 +6,7 @@ export default function PromptCard({handleCardClick, cardInfo, bgImg, isLoading}
         <div className="cursor-pointer w-[240px] aspect-square relative rounded-2xl overflow-hidden bg-white" onClick={handleCardClick}>
             {<Image
                 sizes="100%"
-                src={'/card-1.jpg'} fill alt="cover" priority style={{objectFit: 'cover'}}/>}
+                src={'/card-2.jpg'} fill alt="cover" priority style={{objectFit: 'cover'}}/>}
             <div className={"w-full h-full absolute flex items-center justify-center "+styles['card-info']}>
                 {!isLoading?
                     <h3 className="font-tertiary uppercase font-semibold text-xl text-center text-white">{cardInfo? cardInfo:'Test'}</h3>
